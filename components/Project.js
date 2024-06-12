f4ec74b4fea91daca7f3865dcc16d541389ec120
Niall Murray <98766079+Niall-J-Murray@users.noreply.github.com>
2024-06-12 20:12:57 +0100

    Update Project.js

--- components/Project.js
+++ components/Project.js
@@ -2,7 +2,7 @@ export default function Project() {
   return (
     <>
       <div className={"project"}>
-             <a href="https://slipstreamf1-production.up.railway.app/home">
+             <a href="https://slipstreamf1-production.up.railway.app/slipstream/home">
         <img src="customLogoBlack.png" alt="Slipstream F1 Logo" className={"project-logo"}/>
       </a>
       </div>
