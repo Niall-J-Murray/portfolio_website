a72b55ca254a0d878908e9aadd23a64f39c20287
Niall Murray <98766079+Niall-J-Murray@users.noreply.github.com>
2024-06-25 17:15:33 +0100

    Added Apollo script

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,11 @@ export default function Home() {
             <Head>
                 <title>Niall Murray</title>
                 <link rel="icon" href="Nioctocat.png"/>
+                <script>function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+                        o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+                        o.onload=function(){window.trackingFunctions.onLoad({appId:"66425ce43299100438407824"})},
+                        document.head.appendChild(o)}initApollo();
+                </script>
             </Head>
 
             <main>
